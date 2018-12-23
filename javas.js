3062cdae78b8f41a071b0d4e7cf6035160f0c828
piya03 <piyachaudhary50@gmail.com>
2018-12-23 22:07:03 +0530

    fix

---
+++ javas.js
@@ -0,0 +1,16 @@
+const  btn_id=document.getElementById("btn_id")
+const ball_id=document.getElementById("ball_id")
+
+btn_id.addEventListener("click", function(){
+  ball_id.style.animation="ball_roll 10s infinite linear"; 
+  console.log("hello",ball_id);
+});
+ 
+
+
+
+
+
+
+  
+  
