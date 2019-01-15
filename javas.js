c6d4fa19e69b831e2e096ae20f854cb501c277b8
piya03 <piyachaudhary50@gmail.com>
2019-01-15 22:52:09 +0530

    adding final casino

--- javas.js
+++ javas.js
@@ -1,16 +1,24 @@
-const  btn_id=document.getElementById("btn_id")
-const ball_id=document.getElementById("ball_id")
+const circle1 = document.getElementsByClassName("circle1")[0];
+const circle2 = document.getElementsByClassName("circle2")[0];
+const circle3 = document.getElementsByClassName("circle3")[0];
+const btn_id = document.getElementById("btn_id");
+function getRandomNo(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+let currentRotatedDegree = 0
 
-btn_id.addEventListener("click", function(){
-  ball_id.style.animation="ball_roll 10s infinite linear"; 
-  console.log("hello",ball_id);
-});
- 
+btn_id.onclick = function() {
+  const randomCount = getRandomNo(4, 8);
+  const randomDegree = getRandomNo(20, 361);
+ const randomTime = getRandomNo(1.5, 4)
+  // console.log("ffffffffff", circle1.style)
+  circle1.style.transition = `all ${randomTime}s cubic-bezier(.57, .96, .25, .93)`;
+  circle2.style.transition = `all ${randomTime}s cubic-bezier(.57, .96, .25, .93)`;
+  circle3.style.transition = `all ${randomTime}s cubic-bezier(.57, .96, .25, .93)`;
 
+  circle1.style.transform = `rotate(${currentRotatedDegree + (randomCount * randomDegree)}deg)`;
+  circle2.style.transform = `rotate(${currentRotatedDegree + (randomCount * randomDegree)}deg)`;
 
-
-
-
-
-  
-  
+  circle3.style.transform = `rotate(${currentRotatedDegree + (randomCount * randomDegree)}deg)`;
+  currentRotatedDegree = (randomCount * randomDegree)
+};
